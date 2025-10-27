856884d05028ed82c9247015720496c09ae71a14
xahra11 <149987369+xahra11@users.noreply.github.com>
2025-10-27 10:48:53 -0400

    get ready

---
+++ spell.c
@@ -0,0 +1,6 @@
+
+//finding and opening all the specified files, including directory traversal
+
+//reading the file and generating a sequence of position-annotated words
+
+//checking whether a word is contained in the dictionary
