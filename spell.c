fa6c7e0bff0afd23008aa3989306706663acefaf
Jazmin A. Delos Santos <jad629@java.cs.rutgers.edu>
2025-10-30 12:34:57 -0400

    z

--- spell.c
+++ spell.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h> 
+#include <unistd.h> 
+#include <stdbool.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
 //finding and opening all the specified files, including directory traversal
 
@@ -14,18 +19,30 @@ int main(int argc, char *argv[]) { // spell [-s {suffix}] {dictionary} [{file or
     // one or more inputs
 
     if(argc < 2){
-        printf("Not enough arguments. Please specify a dictionary file and a file/directory to be read from.");
-        return 1;
+        fprintf(stderr, "Not enough arguments. Please specify a dictionary file and a file/directory to be read from.");
+        exit(EXIT_FAILURE);
     }
 
-    bool suffixExists = false
+    bool suffixExists = false 
+    char *suffix = ".txt"; // default suffix
+    int index = 1; 
 
-    if(strcmp(argv[0], "-s") == 0){
-        char suffix[] = argv[1]; // if an argument begins with -s, then the next argument will specify the file name suffix to be used when scanning directories
+    if(strcmp(argv[1], "-s") == 0){
+        if(argc < 4){
+            fprintf(stderr, "Not enough arguments. Please also specify a dictionary file and a file/directory to be read from.");
+            exit(EXIT_FAILURE);
+        }
+        suffix = argv[2]; // if an argument begins with -s, then the next argument will specify the file name suffix to be used when scanning directories
         suffixExists = true;
+        index = 3; // if there is a suffix argument, -s and suffix take up first two arguments, so dictionary starts at third
+    }
 
-    }else{
-        char suffix[] = ".txt";
+    char *dictPath = argv[index];
+
+    int dict_fd = open(dictPath, O_RDONLY);
+    if (dict_fd < 0) {
+        perror(dictPath);
+        exit(EXIT_FAILURE);
     }
 
     for(int i = 1; i < argc; i++){
@@ -35,11 +52,4 @@ int main(int argc, char *argv[]) { // spell [-s {suffix}] {dictionary} [{file or
         }
     }
 
-    if(suffixExists){ // if there is a suffix argument, -s and suffix take up first two arguments
-        char dictionary[] = argv[2]
-    }else{
-        char dictionary[] = argv[0]
-    }
-
-    
 }
